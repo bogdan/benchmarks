a9f9f6d48e8769cc6a9a582c2a4a7fe5658d493a
Bogdan Gusiev <agresso@gmail.com>
2012-05-25 15:51:26 +0300

    do

--- journey_url_generator.rb
+++ journey_url_generator.rb
@@ -1,32 +1,45 @@
 require "diffbench"
 
-# After patch
-/\A\/account\/((?-mix:twitter|github))\/callback(?:\.([^\/.?]+))?\Z/
-
 $:.unshift("./lib")
 
 require "journey"
+require 'perftools'
 
+GC.disable
 routes    = Journey::Routes.new
 router    = Journey::Router.new(routes, {})
 formatter = Journey::Formatter.new(routes)
 
-def add_routes router, paths
+def add_routes router, paths, name
   paths.each do |path|
     path  = Journey::Path::Pattern.new path
-    router.routes.add_route nil, path, {}, {}, {}
+    router.routes.add_route nil, path, {}, {}, name
   end
 end
 add_routes router, [
   Journey::Router::Strexp.new("/foo/:id(.:format)", { :id => /\d+/ }, ['/', '.', '?'], false)
-]
+], "r1"
+add_routes router, [
+  Journey::Router::Strexp.new("/pages/*page(.:format)", {}, ['/', '.', '?'], false)
+], "r2"
 
+#PerfTools::CpuProfiler.start("/tmp/formatter") do
+  #100000.times do
+    #formatter.generate(:path_info, "r1", { :id => '10' }, { })
+  #end
+#end
 
 DiffBench.bm do
   report "generate" do
-    100.times do
+    1000.times do
       formatter.generate(:path_info, nil, { :id => '10' }, { })
+    end
+  end
+  report "generate with globbing" do
+    1000.times do
+      formatter.generate(:path_info, nil, { :page => 'faq' }, { })
     end
   end
 end
 
+GC.enable
